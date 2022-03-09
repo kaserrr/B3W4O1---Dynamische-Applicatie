c18a5eedff689e64e945a93e53247af3a4cd6c2e
kaserrr <99057099@mydavinci.nl>
2022-03-09 10:45:57 +0100

    update 1

--- index.php
+++ index.php
@@ -1,14 +1,40 @@
+<?php 
+include_once 'config.php';
+$characters = $db->query("SELECT * FROM characters");
+$count = $db->query("SELECT COUNT(*) FROM characters");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>All Characters</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="./resources/css/style.css" rel="stylesheet"/>
 </head>
 <body>
-    <?php
-    
-    ?>
+<header><h1>Alle <?php ?> characters uit de database</h1>
+</header>
+    <div id="container">
+    <?php foreach ($characters as $row) { ?>
+        <a class="item" href="character.html">
+            <div class="left">
+                <img class="avatar" src="resources/images/<?php echo $row['avatar'] ?>">
+            </div>
+            <div class="right">
+                <h2><?php echo $row['name'] ?></h2>
+                <div class="stats">
+                    <ul class="fa-ul">
+                        <li><span class="fa-li"><i class="fas fa-heart"></i></span><?php echo $row['health'] ?></li>
+                        <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span><?php echo $row['attack'] ?></li>
+                        <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span><?php echo $row['defense'] ?></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="detailButton"><i class="fas fa-search"></i> bekijk</div>
+        </a>
+        <?php } ?>
+    </div>
+<footer>&copy; Kas Slis 2022</footer>
 </body>
 </html>
